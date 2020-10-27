12414eabefb66690aed0642ad957c99694ac8648
Joe Gallegos <j.gallegos25u@gmail.com>
2020-10-27 13:53:53 -0500

    init commit

---
+++ components/layout.js
@@ -0,0 +1,66 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+const name = 'Joe Gallegos';
+export const siteTitle = 'Next.js blog site';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className="max-w-xl px-4 mt-12 mx-auto mb-24">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Personal blog website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className="flex flex-col items-center">
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className="w-64 h-64 rounded-full"
+              alt={name}
+            />
+            <h1 className="text-3xl leading-5 font-extrabold tracking-tighter py-4">
+              {name}
+            </h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <img
+                  src="/images/profile.jpg"
+                  className="w-24 h-24 rounded-full"
+                  alt={name}
+                />
+              </a>
+            </Link>
+            <h2 className="text-2xl leading-5 my-4">
+              <Link href="/">
+                <a className="text-current">{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className="m-12 text-blue-600">
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Date from '../../components/Date';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className="text-3xl leading-5 font-extrabold tracking-tighter my-4">
+          {postData.title}
+        </h1>
+        <div className="text-gray-500">
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
